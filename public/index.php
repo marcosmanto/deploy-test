3dd58bd4a38386f691ffdcdb76d9ffeb3b1fe847
Marcos Vieira Mantovani Filho <marcos.filho@antt.gov.br>
2019-02-27 14:52:49 -0300

    Changed index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
+
 require '../vendor/autoload.php';
 use Kint\Kint;
 use Carbon\Carbon;
+
 echo '<h1>Deploy me!</h1>';
-printf("Now: %s", Carbon::now()->isoFormat('D/MM/Y'));
+printf('Now: %s', Carbon::now()->isoFormat('D/MM/Y'));
 
 Kint::dump($_SERVER);
+
+$whoops = new Whoops\Run();
+$whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
+$whoops->register();
+
+echo 2 / 0;
