89d01c0f09cb8ea3968a7c54aaf870509193eec7
Marcos Vieira Mantovani Filho <marcos.filho@antt.gov.br>
2019-02-27 15:17:26 -0300

    Changed order of whoops

--- public/index.php
+++ public/index.php
@@ -8,10 +8,10 @@ echo '<h1>Deploy me!</h1>';
 printf('Now: %s', Carbon::now()->isoFormat('D/MM/Y'));
 echo '<p>Change 15:03</p>';
 
-Kint::dump($_SERVER);
-
 $whoops = new Whoops\Run();
 $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
 $whoops->register();
+
+Kint::dump($_SERVER);
 
 echo 2 / 0;
