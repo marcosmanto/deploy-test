d104559971c6dff52f91904b55970855720aa094
Marcos Vieira Mantovani Filho <marcos.filho@antt.gov.br>
2019-02-27 15:40:19 -0300

    Changed index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 
 echo '<h1>Deploy me!</h1>';
 printf('Now: %s', Carbon::now()->isoFormat('D/MM/Y'));
-echo '<p>Change 15:25</p>';
+echo '<p>Change 15:40</p>';
 
 $whoops = new Whoops\Run();
 $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
